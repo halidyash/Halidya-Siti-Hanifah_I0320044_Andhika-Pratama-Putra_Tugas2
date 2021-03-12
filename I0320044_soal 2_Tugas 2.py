3f2ccb8bff6e3f1ba6ac2874f90f83bd97000a88
halidya siti hanifah <halidyahani@gmail.com>
2021-03-12 23:00:01 +0700

    commit pertama

---
+++ I0320044_soal 2_Tugas 2.py
@@ -0,0 +1,35 @@
+#Identitas Pribadi
+print("--------------------------------------")
+print("           Identitas Pribadi          ")
+print("--------------------------------------")
+
+nama    = "Nama Lengkap                    = Halidya Siti Hanifah"
+nim     = "NIM                             = I0320044"
+ttl     = "Tempat/tanggal lahir            = Cianjur, 6 Juli 2001"
+jenis_kelamin = "Jenis Kelamin                   = Perempuan"
+Instansi = "Instansi                        = Universitas Sebelas Maret"
+Fakultas = "Fakultas                        = Teknik Industri"
+Kelas   = "Kelas                           = B"
+tinggal = "h."
+kampung = "Kota Bogor adalah sebuah kota yang berprovinsi di Jawa Barat." \
+        "\nBogor dikenal dengan sebutan Kota Hujan karena memiliki curah hujan yang tinggi." \
+        "\nDi daerah tempat tinggal saya dipenuhi dengan pohon kenari yang sudah berumur ratusan tahun sehingga suasananya sangat sejuk." \
+        "\nDitambah lagi di dekat rumah saya terdapat gor pajajaran sehingga sangat nyaman untuk berolahraga disana."
+umur = ((19*12) + 8)
+tinggi = float(input())
+ukuran_sepatu = int(input())
+
+print(nama)
+print(nim)
+print(ttl)
+print(jenis_kelamin)
+print(Instansi)
+print(Fakultas)
+print(Kelas)
+print(tinggal)
+print(kampung)
+print("Umur saya adalah ", umur, "bulan")
+print("Tinggi saya adalah ",tinggi,"cm")
+print("Ukuran sepatu saya adalah ",ukuran_sepatu)
+
+
